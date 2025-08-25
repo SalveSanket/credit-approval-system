ba693d2e23ab7d95bfd4cbded33da3b86c64ca83
Sanket Salve <sanketsalve01@gmail.com>
2025-08-25 15:47:18 +0530

    remove singuler to plural misspelled error

--- backend/credit_core/settings.py
+++ backend/credit_core/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'customer',
-    'loan',
+    'customers',
+    'loans',
     'ingestion',
 ]
 
